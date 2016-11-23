1eaf463df995f8aa80da366a97eaa2df86ad6d07
b43657 <b43657@freescale.com>
2016-11-23 16:25:03 +0200

    Two motors working, both on left stick
    Signed-off-by: b43657 <b43657@freescale.com>

--- USB_Joystick_v0_1_C_Sharp/Form1.Designer.cs
+++ USB_Joystick_v0_1_C_Sharp/Form1.Designer.cs
@@ -30,17 +30,63 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnGetSerialPorts = new System.Windows.Forms.Button();
+            this.rtbIncoming = new System.Windows.Forms.RichTextBox();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.SendString = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
             // 
+            this.timer1.Interval = 10;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // btnGetSerialPorts
+            // 
+            this.btnGetSerialPorts.Location = new System.Drawing.Point(12, 12);
+            this.btnGetSerialPorts.Name = "btnGetSerialPorts";
+            this.btnGetSerialPorts.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSerialPorts.TabIndex = 0;
+            this.btnGetSerialPorts.Text = "Ports";
+            this.btnGetSerialPorts.UseVisualStyleBackColor = true;
+            this.btnGetSerialPorts.Click += new System.EventHandler(this.btnGetSerialPorts_Click);
+            // 
+            // rtbIncoming
+            // 
+            this.rtbIncoming.Location = new System.Drawing.Point(12, 112);
+            this.rtbIncoming.Name = "rtbIncoming";
+            this.rtbIncoming.Size = new System.Drawing.Size(100, 96);
+            this.rtbIncoming.TabIndex = 1;
+            this.rtbIncoming.Text = "";
+            this.rtbIncoming.TextChanged += new System.EventHandler(this.rtbIncomingData_TextChanged);
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(137, 14);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(121, 21);
+            this.cboPorts.TabIndex = 2;
+            // 
+            // SendString
+            // 
+            this.SendString.Location = new System.Drawing.Point(172, 130);
+            this.SendString.Name = "SendString";
+            this.SendString.Size = new System.Drawing.Size(73, 29);
+            this.SendString.TabIndex = 3;
+            this.SendString.Text = "SendString";
+            this.SendString.UseVisualStyleBackColor = true;
+            this.SendString.Click += new System.EventHandler(this.SendString_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.SendString);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.rtbIncoming);
+            this.Controls.Add(this.btnGetSerialPorts);
             this.Name = "Form1";
             this.Text = "4543";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -51,6 +97,10 @@
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnGetSerialPorts;
+        private System.Windows.Forms.RichTextBox rtbIncoming;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.Windows.Forms.Button SendString;
     }
 }
 
--- USB_Joystick_v0_1_C_Sharp/Form1.cs
+++ USB_Joystick_v0_1_C_Sharp/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using SlimDX.DirectInput;
 using System.Runtime.InteropServices;
+using System.IO.Ports;
+using System.Text;
 
 namespace USB_Joystick_v0_1_C_Sharp
 {
@@ -25,6 +27,7 @@ namespace USB_Joystick_v0_1_C_Sharp
         SlimDX.DirectInput.Joystick stick;
         Joystick[] Sticks;
         bool mouseClicked = false;
+        SerialPort ComPort = new SerialPort();
 
         int yValue = 0;
         int xValue = 0;
@@ -71,30 +74,40 @@ namespace USB_Joystick_v0_1_C_Sharp
             xValue = state.X;
             zValue = state.Z;
 
-            MouseMove(xValue, yValue);
+            //Sending Joystick data over USB2UART
+            //MouseMove(xValue, yValue);
+            SendJoystickData(xValue, yValue);
             bool[] buttons = state.GetButtons();
 
             if (id == 0)
             {
-                if (buttons[0])
+                for (int i = 0; i< buttons.Length; i++)
                 {
-                    // Do stuff
-                    if (mouseClicked == false)
+                    if (buttons[i])
                     {
-                        mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
-                        mouseClicked = true;
+                        SendButtonsData(i);
+                        //MessageBox.Show(String.Format("Button '{0}' pressed", i));
                     }
                 }
-                else
-                {
-                    if (mouseClicked == true)
-                    {
-                        mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
-                        mouseClicked = false;
+            //    if (buttons[0])
+            //    {
+            //        Do stuff
+            //        if (mouseClicked == false)
+            //        {
+            //            mouse_event(MOUSEEVENT_LEFTDOWN, 0, 0, 0, 0);
+            //            mouseClicked = true;
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (mouseClicked == true)
+            //        {
+            //            mouse_event(MOUSEEVENT_LEFTUP, 0, 0, 0, 0);
+            //            mouseClicked = false;
 
-                    }
+                //        }
+                //    }
                 }
-            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -104,18 +117,171 @@ namespace USB_Joystick_v0_1_C_Sharp
 
         public void MouseMove(int posx, int posy)
         {
-            Cursor.Position = new Point(Cursor.Position.X + posx/3, Cursor.Position.Y + posy/3);
-            Cursor.Clip = new Rectangle(this.Location, this.Size);
+            //Cursor.Position = new Point(Cursor.Position.X + posx/3, Cursor.Position.Y + posy/3);
+            //Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
-        private void timer1_Tick(object sender, EventArgs e)
-        {
+        //private void timer1_Tick(object sender, EventArgs e)
+        //{
 
-        }
+        //}
 
         private void timer1_Tick_1(object sender, EventArgs e)
         {
             for (int i = 0; i < Sticks.Length; i++)
                 stickHandle(Sticks[i], i);
+        }
+
+        private void btnGetSerialPorts_Click(object sender, EventArgs e)
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            ArrayComPortsNames = SerialPort.GetPortNames();
+            do
+            {
+                index += 1;
+                rtbIncoming.Text += ArrayComPortsNames[index] + "\n";
+            }
+            while (!((ArrayComPortsNames[index] == ComPortName) ||
+                                (index == ArrayComPortsNames.GetUpperBound(0))));
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+
+            //get first item print in text
+            cboPorts.Text = ArrayComPortsNames[0];
+            cboPorts.DataSource = ArrayComPortsNames;
+
+            if (cboPorts.SelectedIndex > -1)
+            {
+                //MessageBox.Show(String.Format("You selected port '{0}'", cboports.SelectedItem));
+                //Connect(cboPorts.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select a port first");
+            }
+        }
+
+        private void rtbIncomingData_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SendButtonsData(int buttNr)
+        {
+            // Instantiate the communications
+            // port with some basic settings
+
+            //String portName = cboPorts.SelectedItem.ToString();
+            if (cboPorts.SelectedItem == null)
+            {
+                //MessageBox.Show(String.Format("Port is not selected"));
+                return;
+            }
+
+            SerialPort port = new SerialPort(cboPorts.SelectedItem.ToString(), 115200, Parity.None, 8, StopBits.One);
+
+            port.Open();
+            // Write a string
+            //port.Write("Hello World");
+            //port.Write(buttNr.ToString()  + "\r\n");
+
+            String strToSend = buttNr.ToString();
+            port.Write(0xFF + strToSend.Length.ToString() + strToSend);
+
+            // Write a set of bytes
+            //port.Write(new byte[] { 0x0A, 0xE2, 0xFF }, 0, 3);
+
+            // Close the port
+            port.Close();
+        }
+        private void SendJoystickData(int posx, int posy)
+        {
+            // Instantiate the communications
+            // port with some basic settings
+
+            //String portName = cboPorts.SelectedItem.ToString();
+            if (cboPorts.SelectedItem == null)
+            {
+                //MessageBox.Show(String.Format("Port is not selected"));
+                return;
+            }
+
+            SerialPort port = new SerialPort(cboPorts.SelectedItem.ToString(), 115200, Parity.None, 8, StopBits.One);
+
+            // Open the port for communications
+            if (port == null)
+            {
+                //MessageBox.Show(String.Format("Port is not selected"));
+                return;
+            }
+            port.Open();
+
+            // Write a string
+            //port.Write("Hello World");
+            int nrBytes = posx.ToString().Length + posy.ToString().Length;
+
+            //port.Write("7"+ "8" + "\t" + posx.ToString() + "\t" + posy.ToString() + "\r\n");
+            if (posx > 10)
+                posx = 1;
+            else if(posx < -10)
+                posx = 2;
+            else // Idle position
+                posx = 0;
+            if (posy < -10)
+                posy = 1;
+            else if(posy > 3)
+                posy = 3;
+            else
+                posy = 0;
+
+            //String strToSend = "8" + "\t" + posx.ToString() + "\t" + posy.ToString();
+            //String strToSend = "8" + "\t" + posx.ToString() + "\t" + posy.ToString();
+            String strToSend = "8" +  posx.ToString() + posy.ToString();
+
+            //port.Write("8" + "\t" + posx.ToString() + "\t" + posy.ToString() + "\r\n");
+            port.Write(0xFF + strToSend.Length.ToString() + strToSend);
+
+            // Write a set of bytes
+            //port.Write(new byte[] { 0x0A, 0xE2, 0xFF }, 0, 3);
+
+            // Close the port
+            port.Close();
+        }
+
+        private void SendString_Click(object sender, EventArgs e)
+        {
+            // Instantiate the communications
+            // port with some basic settings
+
+            //String portName = cboPorts.SelectedItem.ToString();
+            if(cboPorts.SelectedItem == null)
+            {
+                MessageBox.Show(String.Format("Port is not selected"));
+                return;
+            }
+
+            SerialPort port = new SerialPort(cboPorts.SelectedItem.ToString(), 115200, Parity.None, 8, StopBits.One);
+
+            // Open the port for communications
+            //if (port==null)
+            //{
+            //    MessageBox.Show(String.Format("Port is not selected"));
+            //    return;
+            //}
+            port.Open();
+
+            // Write a string
+            port.Write("Hello World");
+
+            // Write a set of bytes
+            port.Write(new byte[] { 0x0A, 0xE2, 0xFF }, 0, 3);
+
+            // Close the port
+            port.Close();
         }
     }
 }
